3d5645e0a7bec17276a7fe9a7220792ea04c6cf6
kyvminfor <Informaticakyvm@gmail.com>
2024-05-19 18:02:46 -0400

    MIhracion a Oracle y foto gerente listo

--- pagina_empresa/settings.py
+++ pagina_empresa/settings.py
@@ -77,12 +77,24 @@ WSGI_APPLICATION = 'pagina_empresa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/XE',
+        'USER': 'kyvm',
+        'PASSWORD': 'jorge123',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
+    },
+
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
